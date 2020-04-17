ff674b6d072df5346b799be8da41b2e46db105e3
cheun0327 <48849608+cheun0327@users.noreply.github.com>
2020-04-18 00:37:00 +0900

    file create and lseek

---
+++ 3.1create.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(void){
+	int fd;
+	char buf1[] = "Test1 data";
+	char buf2[] = "Test2 data";
+
+	if((fd == creat("test.txt", S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH)) < 0){
+		printf("create error");
+		return 1;
+	}
+	printf("%d",fd);
+	write(fd, buf1, 10);
+	if(lseek(fd, 6L, SEEK_SET) == -1){
+		printf("lseek error");
+		return 2;
+	}
+	write(fd, buf2, 10);
+
+	return 0;
+}
